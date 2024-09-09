9c73412bd09d053269c3c4a9240fed9438898c65
romirkul <ommirkulr@gmail.com>
2024-09-09 13:19:49 +0500

    Small changes

--- commands/helper.py
+++ commands/helper.py
@@ -4,3 +4,4 @@ def run():
     print("  help          Show this help message")
     print("  init          Initialize Pit on current directory")
     print("  add           Add files to the index")
+    
